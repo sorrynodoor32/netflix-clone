1b7fcf924b2b7e9bdc22a0c437d02321d70ea893
sorrynodoor32 <89461242+sorrynodoor32@users.noreply.github.com>
2024-04-14 10:39:32 +0700

    #1

---
+++ pages/auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/netflix-bg.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50 text-white">Hello</div>
+        </div>
+    )
+}
+
+export default Auth
